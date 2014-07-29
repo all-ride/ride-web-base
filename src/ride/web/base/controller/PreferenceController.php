517f0206f060a820d3fd525896142ed50a0a08e3
Joris Vandeweerd <joris@statik.be>
2014-07-29 16:57:35 +0200

    added preference for the image library

--- src/ride/web/base/controller/PreferenceController.php
+++ src/ride/web/base/controller/PreferenceController.php
@@ -19,15 +19,31 @@ class PreferenceController extends AbstractController {
     public function indexAction(ThemeModel $themeModel) {
         $translator = $this->getTranslator();
 
+        // read the themes
         $themes = $themeModel->getThemes();
         foreach ($themes as $themeName => $theme) {
             $themes[$themeName] = $theme->getDisplayName();
         }
 
+        // read the image libraries
+        $imageLibraries = $this->dependencyInjector->getContainer()->getDependencies('ride\\library\\image\\Image');
+        foreach ($imageLibraries as $imageLibraryName => $imageLibraryDependency) {
+            $imageLibraryClass = $imageLibraryDependency->getClassName();
+            try {
+                $imageLibrary = new $imageLibraryClass;
+
+                $imageLibraries[$imageLibraryName] = $imageLibraryName;
+            } catch (Exception $exception) {
+                unset($imageLibraries[$imageLibraryName]);
+            }
+        }
+
+        // build the form
         $data = array(
             'title' => $this->config->get('system.name', 'Ride'),
-            'session-timeout' => $this->config->get('system.session.timeout', 1800) / 60,
             'theme' => $this->config->get('template.theme'),
+            'session-timeout' => $this->config->get('system.session.timeout', 1800) / 60,
+            'image' => $this->config->get('system.image'),
         );
 
         $form = $this->createFormBuilder($data);
@@ -53,8 +69,14 @@ class PreferenceController extends AbstractController {
                 'minmax' => array("minimum" => 0),
             ),
         ));
-
+        $form->addRow('image', 'select', array(
+            'label' => $translator->translate('label.image.library'),
+            'description' => $translator->translate('label.image.library.description'),
+            'options' => $imageLibraries,
+        ));
         $form = $form->build();
+
+        // handle the form
         if ($form->isSubmitted()) {
             try {
                 $form->validate();
@@ -64,6 +86,7 @@ class PreferenceController extends AbstractController {
                 $this->config->set('template.theme', $data['theme']);
                 $this->config->set('system.name', $data['title']);
                 $this->config->set('system.session.timeout', $data['session-timeout'] * 60);
+                $this->config->set('system.image', $data['image']);
 
                 $this->addSuccess('success.preferences.saved');
 
